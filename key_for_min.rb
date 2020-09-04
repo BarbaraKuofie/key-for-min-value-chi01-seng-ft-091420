b13105f55692766eb3cad280a15b967dd8787f66
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-04 19:05:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,9 @@ def key_for_min_value(name_hash)
 min_value = 0 
 min_value_key = ""
 name_hash.each do |key, value|
-  if 
-
+  min_value = name_hash[value][0]
+  if min_value < value
+    min_value_key = key 
+ end
+min_value_key
 end
